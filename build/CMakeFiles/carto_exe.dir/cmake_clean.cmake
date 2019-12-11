1678e360b2269567dbf5d5dd8b626f1068dd054a
ColinBaumgard <colin.baumgard@gmail.com>
2019-12-11 15:32:42 +0100

    initial
    init

---
+++ build/CMakeFiles/carto_exe.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/carto_exe.dir/src/main.cpp.o"
+  "carto_exe.pdb"
+  "carto_exe"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/carto_exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/colin/Documents/ENSTA/C++/CartoGraph
+# Build directory: /home/colin/Documents/ENSTA/C++/CartoGraph/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
